db0fbf40051e3fdff5280dde345f329ab88804cb
Stoffels84 <info@solutionsforyou.be>
2025-08-06 10:55:13 +0200

    Update dashboard_schade.py

--- dashboard_schade.py
+++ dashboard_schade.py
@@ -76,7 +76,6 @@ if generate_pdf:
     # Filter schadegevallen van de gekozen coach
   schade_pdf = df_filtered[df_filtered["teamcoach"] == pdf_coach][["Datum", "volledige naam", "Locatie", "Bus/ Tram", "Link"]].copy()
     schade_pdf = schade_pdf.sort_values(by="Datum")
-
     buffer = BytesIO()
     doc = SimpleDocTemplate(buffer, pagesize=A4)
     styles = getSampleStyleSheet()
